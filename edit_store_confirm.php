e4f26849b7735f848197c1336719f043aeec2f62
charlie0227 <charlie840227@yahoo.com.tw>
2016-07-02 22:49:27 +0800

    20160702-2
    edit not yet

--- edit_store_confirm.php
+++ edit_store_confirm.php
@@ -5,17 +5,13 @@ $s_phone=$_POST['phone'];
 $s_address=$_POST['address'];
 $s_content=$_POST['content'];
 $data = new stdClass();
-/*if ($_FILES["files"]["error"] > 0)
-  {
-  echo "Error: " . $_FILES["files"]["error"] . "<br />";
-  }
-else
-  {
-  echo "Upload: " . $_FILES["files"]["name"] . "<br />";
-  echo "Type: " . $_FILES["files"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["files"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["files"]["tmp_name"];
-  }*/
+function find_store_id($db,$name,$phone,$address,$content/**/){
+	$sql = "SELECT * FROM `jangsc27_cs_project`.`store` where `name`=? and `phone`=? and `address`=? and `content`=?";
+	$sth = $db->prepare($sql);
+	$sth->execute(array($name,$phone,$address,$content));
+	//echo '***'.$sth->fetchObject()->id;
+	return $sth->fetchObject()->id;
+}
 
 if(isset($_POST['name'])){
 	//check repeat
@@ -30,36 +26,75 @@ if(isset($_POST['name'])){
 	}
 }
 //edit image 
-if(isset($_FILES['files'])){
-	$size = getimagesize($_FILES['files']['tmp_name']);
-    $type = $size['mime'];
-    $size = $size[3];
-    $name = $_FILES['files']['name'];
-	$imgfp = base64_encode(file_get_contents($_FILES['files']['tmp_name']));
-	$store_id=find_store_id($db,$s_name,$s_phone,$s_address,$s_content/**/);
-	
-    $sql = "UPDATE `jangsc27_cs_project`.`image` SET `image_type` = ?,`image` = ?,`image_size` = ?,`image_name` = ? WHERE `store_id` =?";
-	$sth = $db->prepare($sql);
-	
-    $sth->bindParam(1, $type);
-	$sth->bindParam(2, $imgfp, PDO::PARAM_LOB);
-	$sth->bindParam(3, $size);
-	$sth->bindParam(4, $name);
+if($_FILES["files"]["name"]!=NULL){
+	//add image 
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["files"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["files"]["tmp_name"]);
+		if($check !== false) {
+			$uploadOk = 1;
+		} else {
+			$data->error="File is not an image.";
+			$uploadOk = 0;
+		}
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		date_default_timezone_set("Asia/Taipei");
+		$target_file=str_replace(".".$imageFileType,"-".date(ymdhis)."-".rand(0,9).".".$imageFileType,$target_file);
+		$uploadOk = 1;
+	}
+	// Check file size
+	if ($_FILES["files"]["size"] > 500000) {
+		$data->error=$data->error."Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+		$data->error=$data->error."Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		$uploadOk = 0;
+	}
+	//content to DB
+	if($uploadOk==1){
+		$size = getimagesize($_FILES['files']['tmp_name']);
+		$type = $size['mime'];
+		$size = $size[3];
+		$name = $_FILES['files']['name'];
+		$imgfp = $target_file;
+		
+		$sql = "INSERT INTO `jangsc27_cs_project`.`image` (image_type,image,image_size,image_name,store_id) VALUES(?,?,?,?,?)";
+		$sth = $db->prepare($sql);
+		
+		$sth->bindParam(1, $type);
+		$sth->bindParam(2, $imgfp,);
+		$sth->bindParam(3, $size);
+		$sth->bindParam(4, $name);
 	//$sth->bindParam(5, $store_id);
 	
 	$sth->execute(array($type,$imgfp,$size,$name,$_POST['edit_id']));
-	//echo $type.$imgfp.$size.$name.'111'.$store_id;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		$data->error=$data->error."Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["files"]["tmp_name"], $target_file)) {
+			chmod($target_file, 0644); 
+			$data->message="The file ". basename( $_FILES["files"]["name"]). " has been uploaded.";
+		} else {
+			$data->error=$data->error."Sorry, there was an error uploading your file.";
+		}
 	}
 
-//header('Location:index.php');
-function find_store_id($db,$name,$phone,$address,$content/**/){
-	$sql = "SELECT * FROM `jangsc27_cs_project`.`store` where `name`=? and `phone`=? and `address`=? and `content`=?";
-	$sth = $db->prepare($sql);
-	$sth->execute(array($name,$phone,$address,$content));
-	//echo '***'.$sth->fetchObject()->id;
-	return $sth->fetchObject()->id;
 }
+//header('Location:index.php');
 
-$data = $_POST['edit_id'];
-echo $data;
+$data->p=$_POST['edit_id'];
+
+echo json_encode($data);
 ?>
