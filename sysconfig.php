2172b5229295f31d1faca2b5c88572ec84c019ac
charlie0227 <charlie840227@yahoo.com.tw>
2016-08-02 21:42:18 +0800

    20160802-2
    GOOD

--- sysconfig.php
+++ sysconfig.php
@@ -7,6 +7,7 @@ $db_user = "jangsc27_cs";
 $db_password = "1234";
 $dsn = "mysql:host=$db_host;dbname=$db_name";
 $db = new PDO($dsn, $db_user, $db_password);
+$root_dir ="/net/cs/102/0216207/public_html/";
 #$_SESSION['account'] = account
 #$_SESSION['name'] = name
 #$_SESSION['id'] = id
