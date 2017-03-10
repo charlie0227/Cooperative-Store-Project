26865b347adc0c35b82d78eeacf23dfbe8a1345d
charlie0227 <charlie27.cs02@nctu.edu.tw>
2017-03-10 12:10:34 +0800

    Update sysconfig.php

--- sysconfig.php
+++ sysconfig.php
@@ -1,19 +1,19 @@
 <?php 
-session_save_path("/home/xu3u4tp6/public_html/session_tmp");
+session_save_path("/**/");
 session_start();
-$db_host = "localhost";
-$db_name = "jangsc27_cs_project";
-$db_user = "charlie27";
-$db_password = "12345678";
+$db_host = "/**/";
+$db_name = "/**/";
+$db_user = "/**/";
+$db_password = "/**/";
 $dsn = "mysql:host=$db_host;dbname=$db_name";
 $db = new PDO($dsn, $db_user, $db_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-$root_dir ="/home/xu3u4tp6/public_html/";
+$root_dir ="/**/";
 date_default_timezone_set("Asia/Taipei");
 function write_log($status,$data)  //狀態 資料       
 {
 	$status='<'.$status.'>';
 	$day = date("Y-m");
-    $URL = "/home/xu3u4tp6/public_html/log/".$day;
+    $URL = $root_dir."log/".$day;
 	$time= "[".date("Y-m-d H:i:s")."] ";
 	//ip
 	if (!empty($_SERVER['HTTP_CLIENT_IP']))
@@ -32,7 +32,4 @@ function write_log($status,$data)  //狀態 資料
     fwrite($fileopen,$log);
     fclose($fileopen); 
 }
-#$_SESSION['account'] = account
-#$_SESSION['name'] = name
-#$_SESSION['id'] = id
 ?>
